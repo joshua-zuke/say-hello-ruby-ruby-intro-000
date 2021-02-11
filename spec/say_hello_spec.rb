80dc9f201dc3501e1dba12affbc2f922d148460d
Joshua Zuke <zuke.joshua@gmail.com>
2021-02-11 17:57:03 +0000

    Automatically backed up by Learn

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -8,7 +8,7 @@ describe "say_hello" do
   end
 
   it 'defaults to Ruby Programmer when no name is passed in' do
-    expect($stdout).to receive(:puts).with("Hello Ruby Programmer!")
+    expect($stdout).to receive(:puts).with("Hello Ruby Programme!")
     say_hello()
   end
 end
